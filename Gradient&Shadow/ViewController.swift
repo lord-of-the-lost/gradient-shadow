e2eee449373cd21a09cdc1e16c8e1abf2dbf2693
Nikolay Ignatov <ignatov.99@yandex.ru>
2023-07-02 17:42:57 +0300

    add view with gradient and shadow

--- Gradient&Shadow/ViewController.swift
+++ Gradient&Shadow/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    private lazy var squareView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 10
+        view.layer.insertSublayer(gradientLayer, at: .zero)
+        view.layer.shadowOpacity = 0.8
+        view.layer.shadowOffset = CGSize(width: 5.0, height: 5.0)
+        view.layer.shadowRadius = 3.0
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.masksToBounds = false
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var gradientLayer: CAGradientLayer = {
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.blue.cgColor, UIColor.red.cgColor]
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.cornerRadius = 10
+        return gradient
+    }()
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        gradientLayer.frame = squareView.bounds
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
+        setupConstraints()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .white
+        view.addSubview(squareView)
     }
 
-
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            squareView.widthAnchor.constraint(equalToConstant: 100),
+            squareView.heightAnchor.constraint(equalToConstant: 100),
+            squareView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            squareView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100)
+        ])
+    }
 }
-
